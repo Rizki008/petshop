4ff7fea318b7c4200a89cff55cdeab8d52f34f50
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-17 08:43:09 +0700

    mengubah number format add produk

--- application/views/layout/backend/produk/v_add.php
+++ application/views/layout/backend/produk/v_add.php
@@ -105,19 +105,3 @@
 	</div>
 </div>
 <!-- / Content -->
-
-<script>
-	function bacaGambar(input) {
-		if (input.files && input.files[0]) {
-			var reader = new FileReader();
-			reader.onload = function(e) {
-				$('#gambar_load').attr('src', e.target.result);
-			}
-			reader.readAsDataURL(input.files[0]);
-		}
-	}
-
-	$("#preview_gambar").change(function() {
-		bacaGambar(this);
-	});
-</script>
