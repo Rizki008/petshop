b9a9d4442bec3ce672fabf50b32de0a55c4439f2
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-19 06:50:12 +0700

    menambahkan hiperlink proses login cart dan cekout

--- application/views/layout/frontend/v_nav.php
+++ application/views/layout/frontend/v_nav.php
@@ -42,45 +42,44 @@
 						</a>
 					</div>
 					<!-- /Wishlist -->
-
+					<?php $keranjang = $this->cart->contents();
+					$jml_item = 0;
+					foreach ($keranjang as $key => $value) {
+						$jml_item = $jml_item + $value['qty'];
+					} ?>
 					<!-- Cart -->
 					<div class="dropdown">
 						<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
 							<i class="fa fa-shopping-cart"></i>
 							<span>Your Cart</span>
-							<div class="qty">3</div>
+							<div class="qty"><?= $jml_item ?></div>
 						</a>
 						<div class="cart-dropdown">
 							<div class="cart-list">
-								<div class="product-widget">
-									<div class="product-img">
-										<img src="./img/product01.png" alt="">
+								<?php foreach ($keranjang as $key => $value) {
+									$produk = $this->m_home->detail_produk($value['id']);
+								?>
+									<div class="product-widget">
+										<div class="product-img">
+											<img src="<?= base_url('assets/gambar/' . $produk->gambar) ?>" alt="">
+										</div>
+										<div class="product-body">
+											<h3 class="product-name"><a href="#"><?= $value['name'] ?></a></h3>
+											<h4 class="product-price"><span class="qty"><?= $value['qty'] ?> x</span>Rp. <?= number_format($value['price'], 0) ?></h4>
+											<h5><i class="fa fa-calculator"></i>Rp. <?= $this->cart->format_number($value['subtotal']); ?></h5>
+										</div>
+										<button class="delete"><i class="fa fa-close"></i></button>
 									</div>
-									<div class="product-body">
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
-									</div>
-									<button class="delete"><i class="fa fa-close"></i></button>
-								</div>
+								<?php } ?>
 
-								<div class="product-widget">
-									<div class="product-img">
-										<img src="./img/product02.png" alt="">
-									</div>
-									<div class="product-body">
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
-									</div>
-									<button class="delete"><i class="fa fa-close"></i></button>
-								</div>
 							</div>
 							<div class="cart-summary">
-								<small>3 Item(s) selected</small>
-								<h5>SUBTOTAL: $2940.00</h5>
+								<small><?= $jml_item ?> Item(s) selected</small>
+								<h5>SUBTOTAL: Rp <?= $this->cart->format_number($this->cart->total()); ?></h5>
 							</div>
 							<div class="cart-btns">
-								<a href="#">View Cart</a>
-								<a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
+								<a href="<?= base_url('belanja') ?>">View Cart</a>
+								<a href="<?= base_url('belanja/cekout') ?>">Checkout <i class="fa fa-arrow-circle-right"></i></a>
 							</div>
 						</div>
 					</div>
