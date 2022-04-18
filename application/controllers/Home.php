ea5a50621f7fb5b95e33bb060669a7b6691e16f6
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-19 06:39:54 +0700

    mengubah view frontend

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,19 +5,51 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home extends CI_Controller
 {
 
-	// public function __construct()
-	// {
-	// 	parent::__construct();
-	// 	//Load Dependencies
-
-	// }
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+		$this->load->model('m_home');
+		// $this->load->model('m_produk');
+		// $this->load->model('m_transaksi');
+	}
 
 	// List all your items
 	public function index()
 	{
 		$data = array(
 			'title' => 'PETSHOP',
+			'produk' => $this->m_home->produk(),
+			'best_deal_product' => $this->m_home->best_deal_product(),
+			'diskon' => $this->m_home->diskon(),
+			'best_deal_product_transaksi' => $this->m_home->best_deal_product_transaksi(),
 			'isi' => 'v_home'
+		);
+		$this->load->view('layout/frontend/v_wrapper', $data, FALSE);
+	}
+
+	public function kategori($id_kategori)
+	{
+		$kategori = $this->m_home->kategori($id_kategori);
+		$data = array(
+			'title' => $kategori->nama_kategori,
+			'produk' => $this->m_home->produk_all($id_kategori),
+			'isi' => 'layout/frontend/kategori/v_kategori_produk'
+		);
+		$this->load->view('layout/frontend/v_wrapper', $data, FALSE);
+	}
+
+
+
+	public function detail_produk($id_produk)
+	{
+		$data = array(
+			'title' => 'Detail Produk',
+			'gambar' => $this->m_home->gambar_produk($id_produk),
+			'produk' => $this->m_home->detail_produk($id_produk),
+			'related_products' => $this->m_home->related_products($id_produk),
+			// 'reviews' => $this->m_home->reviews($id_produk),
+			'isi' => 'layout/frontend/detail/v_detail_produk'
 		);
 		$this->load->view('layout/frontend/v_wrapper', $data, FALSE);
 	}
