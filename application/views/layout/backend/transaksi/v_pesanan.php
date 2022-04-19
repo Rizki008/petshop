d80f5ec82437db98a86908ac26027b1b1e223863
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-20 06:44:43 +0700

    menambahkan view tampilan pesanan masuk

--- application/views/layout/backend/transaksi/v_pesanan.php
+++ application/views/layout/backend/transaksi/v_pesanan.php
@@ -20,9 +20,42 @@
 
 						<div id="accordionOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
 							<div class="accordion-body">
-								Lemon drops chocolate cake gummies carrot cake chupa chups muffin topping. Sesame snaps icing
-								marzipan gummi bears macaroon dragée danish caramels powder. Bear claw dragée pastry topping
-								soufflé. Wafer gummi bears marshmallow pastry pie.
+								<table class="table">
+									<tr>
+										<th>Nama Pelanggan</th>
+										<th>No Order</th>
+										<th>Tanggal Order</th>
+										<th>Expedisi</th>
+										<th>Biaya Pengiriman</th>
+										<th>Total Bayar</th>
+										<th>Action</th>
+									</tr>
+									<?php foreach ($pesanan as $key => $value) { ?>
+										<tr>
+											<td><?= $value->nama_pelanggan ?></td>
+											<td>
+												<?= $value->no_order ?></a>
+											</td>
+											<td><?= $value->tgl_order ?></td>
+											<td><?= $value->expedisi ?><br><?= $value->paket ?><br><?= $value->estimasi ?></td>
+											<td><b>Rp. <?= number_format($value->ongkir, 0) ?></b><br>
+											<td><b>Rp. <?= number_format($value->total_bayar, 0) ?></b><br>
+												<?php if ($value->status_bayar == 0) { ?>
+													<span class="badge badge-warning">Belum bayar</span>
+												<?php } else { ?>
+													<span class="badge badge-success">Sudah bayar</span><br>
+													<span class="badge badge-primary">Menunggu Verifikasi</span>
+												<?php } ?>
+											</td>
+											<td>
+												<?php if ($value->status_bayar == 1) { ?>
+													<a href="<?= base_url('admin/detail/' . $value->no_order) ?>" class=" btn btn-sm btn-success btn-flat">Detail Bayar</a>
+													<a href=" <?= base_url('admin/proses/' . $value->id_transaksi) ?>" class="btn btn-sm btn-flat btn-primary">Verifikasi</a>
+												<?php } ?>
+											</td>
+										</tr>
+									<?php } ?>
+								</table>
 							</div>
 						</div>
 					</div>
@@ -34,23 +67,116 @@
 						</h2>
 						<div id="accordionTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
 							<div class="accordion-body">
-								Dessert ice cream donut oat cake jelly-o pie sugar plum cheesecake. Bear claw dragée oat cake
-								dragée ice cream halvah tootsie roll. Danish cake oat cake pie macaroon tart donut gummies.
-								Jelly beans candy canes carrot cake. Fruitcake chocolate chupa chups.
+								<table class="table">
+									<tr>
+										<th>Nama Pelanggan</th>
+										<th>No Order</th>
+										<th>Tanggal Order</th>
+										<th>Expedisi</th>
+										<th>Biaya Pengiriman</th>
+										<th>Total Bayar</th>
+										<th>Action</th>
+									</tr>
+									<?php foreach ($pesanan_diproses as $key => $value) { ?>
+										<tr>
+											<td><?= $value->nama_pelanggan ?></td>
+											<td><a href="<?= base_url('admin/detail/' . $value->no_order) ?>"><?= $value->no_order ?></a></td>
+											<td><?= $value->tgl_order ?></td>
+											<td><?= $value->expedisi ?><br><?= $value->paket ?><br><?= $value->estimasi ?></td>
+											<td><b>Rp. <?= number_format($value->ongkir, 0) ?></b><br>
+											<td>
+												<b>Rp. <?= number_format($value->total_bayar, 0) ?></b><br>
+												<span class="badge badge-primary">Dikemas</span>
+											</td>
+											<td><button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#kirim<?= $value->id_transaksi ?>">
+													<i class="bx bx-paper-plane me-1"></i>
+													Kirim
+												</button></td>
+										</tr>
+									<?php } ?>
+								</table>
 							</div>
 						</div>
 					</div>
 					<div class="card accordion-item">
 						<h2 class="accordion-header" id="headingThree">
 							<button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordionThree" aria-expanded="false" aria-controls="accordionThree">
-								Selesai
+								Proses Pengiriman
 							</button>
 						</h2>
 						<div id="accordionThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
 							<div class="accordion-body">
-								Oat cake toffee chocolate bar jujubes. Marshmallow brownie lemon drops cheesecake. Bonbon
-								gingerbread marshmallow sweet jelly beans muffin. Sweet roll bear claw candy canes oat cake
-								dragée caramels. Ice cream wafer danish cookie caramels muffin.
+								<table class="table">
+									<tr>
+										<th>Nama Pelanggan</th>
+										<th>No Order</th>
+										<th>Tanggal Order</th>
+										<th>Expedisi</th>
+										<th>Alamat</th>
+										<th>Biaya Pengiriman</th>
+										<th>No Resi</th>
+										<th>Total Bayar</th>
+									</tr>
+									<?php foreach ($pesanan_dikirim as $key => $value) { ?>
+										<tr>
+											<td><?= $value->nama_pelanggan ?></td>
+											<td><?= $value->no_order ?></td>
+											<td><?= $value->tgl_order ?></td>
+											<td><?= $value->expedisi ?><br><?= $value->paket ?><br><?= $value->estimasi ?></td>
+											<td><?= $value->alamat ?></td>
+											<td><b>Ongkir : Rp. <?= number_format($value->ongkir, 0) ?></b><br>
+											</td>
+											<td><?= $value->no_resi ?></td>
+											<td>
+												<b>Rp. <?= number_format($value->total_bayar, 0) ?></b><br>
+												<span class="badge badge-success">Dikirim</span>
+											</td>
+
+										</tr>
+									<?php } ?>
+
+								</table>
+							</div>
+						</div>
+					</div>
+					<div class="card accordion-item">
+						<h2 class="accordion-header" id="headingFor">
+							<button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#accordionFor" aria-expanded="false" aria-controls="accordionFor">
+								Selesai
+							</button>
+						</h2>
+						<div id="accordionFor" class="accordion-collapse collapse" aria-labelledby="headingFor" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
+								<table class="table">
+									<tr>
+										<th>Nama Pelanggan</th>
+										<th>No Order</th>
+										<th>Tanggal Order</th>
+										<th>Expedisi</th>
+										<th>Alamat</th>
+										<th>Biaya Pengiriman</th>
+										<th>No Resi</th>
+										<th>Total Bayar</th>
+									</tr>
+									<?php foreach ($pesanan_selesai as $key => $value) { ?>
+										<tr>
+											<td><?= $value->nama_pelanggan ?></td>
+											<td><?= $value->no_order ?></td>
+											<td><?= $value->tgl_order ?></td>
+											<td><?= $value->expedisi ?><br><?= $value->paket ?><br><?= $value->estimasi ?></td>
+											<td><?= $value->alamat ?></td>
+											<td><b>Ongkir : Rp. <?= number_format($value->ongkir, 0) ?></b><br>
+											</td>
+											<td><?= $value->no_resi ?></td>
+											<td>
+												<b>Rp. <?= number_format($value->total_bayar, 0) ?></b><br>
+												<span class="badge badge-success">Diterima</span>
+											</td>
+
+										</tr>
+									<?php } ?>
+
+								</table>
 							</div>
 						</div>
 					</div>
@@ -59,3 +185,32 @@
 		</div>
 	</div>
 </div>
+
+<?php foreach ($proses_kirim as $key => $value) { ?>
+	<div class="modal fade" id="kirim<?= $value->id_transaksi ?>" tabindex="-1" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="modalCenterTitle">Kirim Produk <?= $value->no_order ?></h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<?php echo form_open('admin/kirim/' . $value->id_transaksi) ?>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col mb-3">
+							<label for="nameWithTitle" class="form-label">No Resi Pengiriman</label>
+							<input type="text" id="nameWithTitle" name="no_resi" value="<?= $value->no_resi ?>" class="form-control" placeholder="Enter No Resi" required />
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+						Close
+					</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+				<?php echo form_close() ?>
+			</div>
+		</div>
+	</div>
+<?php } ?>
