171f0e1371be1aafc32757711947dc14ab915fdc
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-20 06:45:05 +0700

    menambahkan kategori gambar

--- application/views/layout/backend/kategori/v_kategori.php
+++ application/views/layout/backend/kategori/v_kategori.php
@@ -6,9 +6,9 @@
 		<h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
 		<!-- Bootstrap Table with Header - Dark -->
 		<div class="card">
-			<h5 class="card-header"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
+			<h5 class="card-header"><a href="<?= base_url('kategori/add') ?>" class="btn btn-primary">
 					Tambah Kategori
-				</button>
+				</a>
 			</h5>
 			<div class="table-responsive text-nowrap">
 				<table class="table">
@@ -16,6 +16,7 @@
 						<tr>
 							<th>No</th>
 							<th>Nama Kategori</th>
+							<th>Gambar</th>
 							<th>Actions</th>
 						</tr>
 					</thead>
@@ -25,16 +26,17 @@
 							<tr>
 								<td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong><?= $no++ ?></strong></td>
 								<td><?= $value->nama_kategori ?></td>
+								<td><img src="<?= base_url('assets/gambarkategori/' . $value->gambar) ?>" alt="" width="100px"></td>
 								<td>
 									<div class="dropdown">
 										<button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
 											<i class="bx bx-dots-vertical-rounded"></i>
 										</button>
 										<div class="dropdown-menu">
-											<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalEdit<?= $value->id_kategori ?>">
+											<a href="<?= base_url('kategori/update/' . $value->id_kategori) ?>" class="dropdown-item">
 												<i class="bx bx-edit-alt me-1"></i>
 												Edit
-											</button>
+											</a>
 											<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalDelete<?= $value->id_kategori ?>">
 												<i class="bx bx-trash me-1"></i>
 												Delete
@@ -53,63 +55,6 @@
 </div>
 
 <hr class="my-5" />
-
-<div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="modalCenterTitle">Tambah kategori Produk</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<?php echo form_open('kategori/add') ?>
-			<div class="modal-body">
-				<div class="row">
-					<div class="col mb-3">
-						<label for="nameWithTitle" class="form-label">Nama Kategori</label>
-						<input type="text" id="nameWithTitle" name="nama_kategori" class="form-control" placeholder="Enter Nama Kategori" required />
-					</div>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-					Close
-				</button>
-				<button type="submit" class="btn btn-primary">Save changes</button>
-			</div>
-			<?php echo form_close() ?>
-		</div>
-	</div>
-</div>
-
-
-<?php foreach ($ketegori as $key => $value) { ?>
-	<div class="modal fade" id="modalEdit<?= $value->id_kategori ?>" tabindex="-1" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="modalCenterTitle">Tambah kategori Produk</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<?php echo form_open('kategori/update/' . $value->id_kategori) ?>
-				<div class="modal-body">
-					<div class="row">
-						<div class="col mb-3">
-							<label for="nameWithTitle" class="form-label">Nama Kategori</label>
-							<input type="text" id="nameWithTitle" name="nama_kategori" value="<?= $value->nama_kategori ?>" class="form-control" placeholder="Enter Nama Kategori" required />
-						</div>
-					</div>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-						Close
-					</button>
-					<button type="submit" class="btn btn-primary">Save changes</button>
-				</div>
-				<?php echo form_close() ?>
-			</div>
-		</div>
-	</div>
-<?php } ?>
 
 <?php foreach ($ketegori as $key => $value) { ?>
 	<div class="modal fade" id="modalDelete<?= $value->id_kategori ?>" aria-labelledby="modalToggleLabel" tabindex="-1" style="display: none" aria-hidden="true">
