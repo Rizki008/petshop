88b499169179a1a8864bb0efe29a4642b3a4ceec
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-04-20 06:46:03 +0700

    menambahkan proses pesanan masuk

--- application/controllers/Pesanan_masuk.php
+++ application/controllers/Pesanan_masuk.php
@@ -7,18 +7,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Pesanan_masuk extends CI_Controller
 {
 
-	// public function __construct()
-	// {
-	// 	parent::__construct();
-	// 	//Load Dependencies
-
-	// }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_pesanan_masuk');
+	}
 
 	// List all your items
 	public function index()
 	{
 		$data = array(
 			'title' => 'Pesanan Masuk',
+			'pesanan' => $this->m_pesanan_masuk->pesanan(),
+			'pesanan_diproses' => $this->m_pesanan_masuk->pesanan_diproses(),
+			'pesanan_dikirim' => $this->m_pesanan_masuk->pesanan_dikirim(),
+			'pesanan_selesai' => $this->m_pesanan_masuk->pesanan_selesai(),
+			'proses_kirim' => $this->m_pesanan_masuk->proses_kirim(),
 			'isi' => 'layout/backend/transaksi/v_pesanan'
 		);
 		$this->load->view('layout/backend/v_wrapper', $data, FALSE);
