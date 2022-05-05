9d747ce812966e32493510cf158e124a69106680
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-05 20:21:05 +0700

    mengubah view backend diskon

--- application/views/layout/backend/diskon/v_diskon.php
+++ application/views/layout/backend/diskon/v_diskon.php
@@ -6,9 +6,6 @@
 		<h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> <?= $title ?></h4>
 		<!-- Bootstrap Table with Header - Dark -->
 		<div class="card">
-			<h5 class="card-header"><a href="<?= base_url('diskon/add') ?>" class="btn btn-primary">
-					Tambah Diskon
-				</a>
 			</h5>
 			<div class="table-responsive text-nowrap">
 				<table class="table">
@@ -35,10 +32,10 @@
 											<i class="bx bx-dots-vertical-rounded"></i>
 										</button>
 										<div class="dropdown-menu">
-											<!-- <button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalEdit<?= $value->id_diskon ?>">
+											<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalEdit<?= $value->id_diskon ?>">
 												<i class="bx bx-edit-alt me-1"></i>
 												Edit
-											</button> -->
+											</button>
 											<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalDelete<?= $value->id_diskon ?>">
 												<i class="bx bx-trash me-1"></i>
 												Delete
@@ -57,6 +54,42 @@
 </div>
 
 <hr class="my-5" />
+
+<!-- Modal -->
+<?php foreach ($diskon as $key => $value) { ?>
+	<div class="modal fade" id="modalEdit<?= $value->id_diskon ?>" tabindex="-1" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="modalCenterTitle">Update Diskon</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<?php echo form_open('diskon/update/' . $value->id_diskon) ?>
+					<div class="row">
+						<div class="col mb-3">
+							<label for="nameWithTitle" class="form-label">Nama Diskon</label>
+							<input type="text" id="nameWithTitle" name="nama_diskon" class="form-control" placeholder="Lebaran" />
+						</div>
+					</div>
+					<div class="row g-3">
+						<div class="col mb-0">
+							<label for="emailWithTitle" class="form-label">Harga Diskon</label>
+							<input type="text" id="emailWithTitle" name="diskon" class="form-control" placeholder="10.000.000" />
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+						Close
+					</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+				<?php echo form_close() ?>
+			</div>
+		</div>
+	</div>
+<?php } ?>
 
 <?php foreach ($diskon as $key => $value) { ?>
 	<div class="modal fade" id="modalDelete<?= $value->id_diskon ?>" aria-labelledby="modalToggleLabel" tabindex="-1" style="display: none" aria-hidden="true">
