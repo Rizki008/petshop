277760f6ed716f8a8d8dc5f2312e034c3c303488
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-08-16 09:38:46 +0700

    menambahkan pemilik

---
+++ application/controllers/Pemilik.php
@@ -0,0 +1,96 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pemilik extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_admin');
+		$this->load->model('m_transaksi');
+		$this->load->model('m_pesanan_masuk');
+		$this->load->model('m_laporan');
+	}
+	public function index()
+	{
+		$data = array(
+			'title' => 'Dashboard',
+			'user' => $this->m_admin->user(),
+			'isi' => 'v_pemilik'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+
+	public function laporan()
+	{
+		$data = array(
+			'title' => 'Laporan',
+			'isi' => 'layout/pemilik/laporan/v_laporan'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+
+	public function lap_harian()
+	{
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+
+		$data = array(
+			'title' => 'Laporan Penjualan Harian',
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'laporan' => $this->m_laporan->lap_harian($tanggal, $bulan, $tahun),
+			'isi' => 'layout/pemilik/laporan/v_lap_hari'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+
+	public function lap_bulanan()
+	{
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+
+		$data = array(
+			'title' => 'Laporan Penjualan Bulanan',
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'laporan' => $this->m_laporan->lap_bulanan($bulan, $tahun),
+			'isi' => 'layout/pemilik/laporan/v_lap_bulan'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+
+	public function lap_tahunan()
+	{
+		$tahun = $this->input->post('tahun');
+
+		$data = array(
+			'title' => 'Laporan Penjualan Tahunan',
+			'tahun' => $tahun,
+			'laporan' => $this->m_laporan->lap_tahunan($tahun),
+			'isi' => 'layout/pemilik/laporan/v_lap_tahun'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+
+	public function lap_stock()
+	{
+		$tanggal = $this->input->post('tanggal');
+		$bulan = $this->input->post('bulan');
+		$tahun = $this->input->post('tahun');
+
+		$data = array(
+			'title' => 'Laporan Stock Barang',
+			'tanggal' => $tanggal,
+			'bulan' => $bulan,
+			'tahun' => $tahun,
+			'laporan' => $this->m_laporan->lap_harian($tanggal, $bulan, $tahun),
+			'isi' => 'layout/pemilik/laporan/v_lap_stock'
+		);
+		$this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+	}
+}
