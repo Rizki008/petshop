bf3061c1fde7a87ff9ea9f1c991ee646565be16b
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-09-07 09:17:47 +0700

    menambahkan model laporan keseluruhan

--- application/models/M_laporan.php
+++ application/models/M_laporan.php
@@ -44,6 +44,19 @@ class M_laporan extends CI_Model
         return $this->db->get()->result();
     }
 
+    public function laporan()
+    {
+        $this->db->select('*');
+        $this->db->from('rinci_transaksi');
+        $this->db->join('transaksi', 'transaksi.no_order = rinci_transaksi.no_order', 'left');
+        $this->db->join('produk', 'produk.id_produk = rinci_transaksi.id_produk', 'left');
+        // $this->db->where('YEAR(tgl_order)', $tahun);
+        $this->db->where('status_bayar=1');
+        $this->db->order_by('qty', 'desc');
+
+        return $this->db->get()->result();
+    }
+
     public function lap_stock($tanggal, $bulan, $tahun)
     {
         $this->db->select('*');
